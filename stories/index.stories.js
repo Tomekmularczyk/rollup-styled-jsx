79b27726eec3027f01ad580a8db70613685d2d59
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2019-07-11 19:33:27 +0200

    add storybook with auto setup

---
+++ stories/index.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import HelloWorld from "../src/HelloWorld";
+
+storiesOf("HelloWorld", module).add("default", () => <HelloWorld />);
